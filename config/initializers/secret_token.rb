ae5d56f3160bdcf85e8bd738235b76dd0e65dfd4
Jason B <devilshaircut@gmail.com>
2013-08-11 15:50:58 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Textsprout::Application.config.secret_token = '220a2a3eb79060964e94dea6e63666dfee355c178bba963a2659c1b68402f15f23dd5116a09d9870b4067b97ef56f23b529f490d960b6a26cdda597add397c30'
